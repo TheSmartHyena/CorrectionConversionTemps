810d592709354b19921a5a655afb612c7e949422
Philippe Skopal <skopal.philippe@gmail.com>
2025-12-18 13:24:01 +0100

    Correction exercice sur les masques de chaines

--- UserAbstract/src/Main.java
+++ UserAbstract/src/Main.java
@@ -1,43 +1,11 @@
-import java.util.List;
-
-void main() {
-    Config config = new Config(
-            1,
-            1,
-            List.of(
-                    new Rule(3, "Fizz"),
-                    new Rule(5, "Buzz")
-            )
-    );
-    // int N = 100;
-    int N = Integer.MAX_VALUE;
-
-    FizzBuzzIterator iterator = new FizzBuzzIterator(
-            config.start,
-            N,
-            config.step,
-            config.rules
-    );
-
-    while (iterator.hasNext()) {
-        IO.println(iterator.next());
+static String maskify(String txt) {
+    if (txt.length() <= 4) {
+        return txt;
     }
+
+    return "#".repeat(txt.length() - 4) + txt.substring(txt.length() - 4);
 }
 
-void main1() {
-    int N = 100;
-
-    for (int i = 0; i <= N; i++) {
-        String result = "";
-        if (i % 3 == 0) {
-            result += "Fizz";
-        }
-        if (i % 5 == 0) {
-            result += "Buzz";
-        }
-        if (result.isEmpty()) {
-            result = Integer.toString(i);
-        }
-        System.out.println(result);
-    }
+void main() {
+    System.out.println(maskify("64607935616"));
 }
